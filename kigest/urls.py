5b2a3f245700a4ab2de93b48370e92c32b763b77
kspaseff <kspaseff@mail.ccsf.edu>
2021-09-23 13:04:58 -0400

    urlconf routes all possible strings in the url to the same homepage. currently no bad links possible.

--- kigest/urls.py
+++ kigest/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path, path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls'))
+    re_path(r'.*', include('website.urls'))
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,4 @@ from django.http import HttpResponse
 def homepage(request):
 	return HttpResponse("Welcome to the homepage")
 # Create your views here.
+
