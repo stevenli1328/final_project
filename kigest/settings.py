c49c1405b6829443d1650a47c15e6cda17ad5c79
kspaseff <kspaseff@mail.ccsf.edu>
2021-10-04 17:01:58 -0400

    Saving my work

--- kigest/settings.py
+++ kigest/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ('/home/cabox/workspace/EMS/kigest/website/static')
+STATICFILES_DIRS = ('/home/cabox/workspace/EMS/kigest/website/static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
