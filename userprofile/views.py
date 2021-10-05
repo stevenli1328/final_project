1e52bbf151cd477c56a595afa36139cbfd92c941
kspaseff <kspaseff@mail.ccsf.edu>
2021-10-05 12:32:01 -0400

    Login and Logout working

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 def user_profile(request):
     return render(request, 'userprofile/profile.html', {'name': 'profile'})
@@ -8,6 +9,17 @@ def logout_user(request):
     if request.method == 'POST':
         logout(request)
         return redirect('homepage')
+
+def login_user(request):
+    if request.method == 'GET':
+        return render(request, 'userprofile/login.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'userprofile/login.html', {'form': AuthenticationForm(), 'error': 'Username and password did not match.'})
+        else:
+            login(request, user)
+            return redirect('homepage')
         
 def other(request):
     return HttpResponse("Bad Page!")
