ffe5353d57ae4761a6a766ee67ace77629f08721
kspaseff <kspaseff@mail.ccsf.edu>
2021-10-16 14:31:31 -0400

    more testing added

--- schedule/tests.py
+++ schedule/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from userprofile.models import Employee
+import datetime
 
-# Create your tests here.
+from .models import Schedule
+
+
+class ScheduleModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        employee1 = Employee.objects.create(
+                user=User.objects.create(username='kurt'),
+                dateofbirth=datetime.date(1988, 8, 18))
+
+        schedule = Schedule.objects.create(
+            employee=employee1,
+            schedule_date=datetime.date(2021, 10, 18),
+            time_start=datetime.time(7,0,0),
+            time_end=datetime.time(5,0,0), 
+            date_created=datetime.date(2021, 10, 16)
+        )
+    
+    def test_the_schedule(self):
+        schedule = Schedule.objects.get(id=1)
+        employee = Employee.objects.get(id=1)
+        self.assertEqual(schedule.employee, employee)
--- tasks/tests.py
+++ tasks/tests.py
@@ -1,1 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Task
+from userprofile.models import Employee
+import datetime
+
+
+class TaskModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        assigner1 = Employee.objects.create(
+                user=User.objects.create(username='kurt'),
+                dateofbirth=datetime.date(1988, 8, 18))
+
+        task = Task.objects.create(
+            title='do some things',
+            assigner=assigner1,
+            date_created=datetime.date(2021, 10, 16),
+            description='a bunch of descriptors here'
+        )
+
+        task.assignee.add(assigner1)
+    
+    def test_the_task(self):
+        task = Task.objects.get(id=1)
+        employee = Employee.objects.get(id=1)
+        self.assertEqual(task.assigner, employee)
