892bc02e8dce2d6a1606d51d3adcf78eb656484a
kspaseff <kspaseff@mail.ccsf.edu>
2021-10-18 18:19:52 -0400

    calendar working

--- schedule/views.py
+++ schedule/views.py
@@ -13,7 +13,9 @@ def schedule(request):
     if request.user.is_authenticated:
         current_employee = Employee.objects.get(user=request.user)
         schedules = current_employee.schedule_set.all()
-    return render(request, 'schedule/schedule.html', {'name': 'schedule', 'employee': current_employee, 'schedules': schedules})
+    return render(request, 'schedule/schedule.html', {'name': 'schedule', 
+    'employee': current_employee, 
+    'schedules': schedules})
 
 def createschedule(request):
         if request.method == 'GET':
