6803a64b6c01361f260258596a569294b6f0a86c
kspaseff <kspaseff@mail.ccsf.edu>
2021-10-23 17:49:21 -0400

    stashing changes

--- tasks/tests.py
+++ tasks/tests.py
@@ -3,25 +3,50 @@ from django.contrib.auth.models import User
 from .models import Task
 from userprofile.models import Employee
 import datetime
-
+import random
 
 class TaskModelTest(TestCase):
 
+    #Create 1 assigner employee and 10 new employees. 
+    #Then create ten tasks, each with the same assigner, but each 
+    #with some random number (between 1 and ten) of assignee's.
     @classmethod
     def setUpTestData(self):
         assigner1 = Employee.objects.create(
                 user=User.objects.create(username='kurt'))
 
-        task = Task.objects.create(
-            title='do some things',
-            assigner=assigner1,
-            date_created=datetime.date(2021, 10, 16),
-            description='a bunch of descriptors here'
-        )
+        employees = []
+        tasks = []
 
-        task.assignee.add(assigner1)
+        for i in range(10):
+            employees.append(Employee.objects.create(
+                user=User.objects.create(username='employee number ' + str(i))
+            ))
+
+
+        for i in range(10):
+            tasks.append(Task.objects.create(
+                title='do thing number ' + str(i),
+                assigner=assigner1,
+                description='a bunch of descriptors here'
+            ))
+
+            ass_count = random.randrange(10)
+            print(ass_count)
+            
+            for j in range(ass_count):
+                tasks[i].assignee.add(employees[j]) 
+
+
     
     def test_the_task(self):
-        task = Task.objects.get(id=1)
-        employee = Employee.objects.get(id=1)
-        self.assertEqual(task.assigner, employee)
+
+        for i in range(10): 
+            task = Task.objects.get(id=i + 1)
+            employee = Employee.objects.get(id=i + 1)
+            assignees = task.assignee.all()
+            self.assertEqual(task.assigner, assignees)
+
+
+
+        
