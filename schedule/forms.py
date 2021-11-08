acd1f4ac44f3cd03ea14cb7026bd55f6411db3f6
gwong23 <gwong23@mail.ccsf.edu>
2021-11-08 16:34:09 -0500

    updated new task css

--- schedule/forms.py
+++ schedule/forms.py
@@ -8,5 +8,13 @@ class ScheduleForm(ModelForm):
         model = Schedule
         fields = ['title', 'employee', 'schedule_date', 'time_start', 'time_end']
     
-    schedule_date = forms.DateField(widget=forms.SelectDateWidget)
-    time_start = forms.TimeField(widget=forms.TimeInput)
+#    schedule_date = forms.DateField(widget=forms.SelectDateWidget)
+#    time_start = forms.TimeField(widget=forms.TimeInput)
+
+    widgets = {
+        'title': forms.TextInput(attrs={'class': 'form-control'}),
+        'employee': forms.TextInput(attrs={'class': 'form-control'}),
+        'schedule_date': forms.Select(attrs={'class': 'form-control'}),
+        'time_start': forms.TextInput(attrs={'class': 'form-control'}),
+        'time_end': forms.TextInput(attrs={'class': 'form-control'}),
+    }
